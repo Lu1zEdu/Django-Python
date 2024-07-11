7f121e6977893e0786426ce5f064b91f066aaec3
Lu1zEdu <le6269199@gmail.com>
2024-07-11 00:27:39 -0300

    Let's gooooo

--- receitas/views.py
+++ receitas/views.py
@@ -5,7 +5,16 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, "Index.html")
+    receitas = {
+        1: "Sorvete",
+        2: "Coxinha",
+        3: "Pastel",
+        4: "Bolo",
+        5: "Torta",
+        6: "Sushi",
+    }
+    dados = {"nome_da_receita": receitas}
+    return render(request, "Index.html", dados)
 
 
 def receita(request):
